4068b618330154097969c89edb4153940036f9ab
Sara Valle <svj971@gmail.com>
2021-03-09 14:22:22 -0500

    feat: add database connection into server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
+const database = require('./src/db/connection');
 
 //body parser middleware
 app.use(express.json());
@@ -11,7 +12,9 @@ app.use('/', (req, res) => {
     res.send('Home');
 });
 
-function init() {
+async function init() {
+    await database.connection();
+
     app.listen(PORT, () => {
         console.log(chalk.blue.bold(`Server on port http://localhost:${PORT}`));
     });
