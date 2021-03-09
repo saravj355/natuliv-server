e30b52aa66325abdacab61cc5aed573587e47f14
Sara Valle <svj971@gmail.com>
2021-03-09 14:20:25 -0500

    add sequelize configuration to models

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,8 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const { Config } = require('../config/db');
 
 const sequelize = new Sequelize({
-    dialect: 'mysql',
+    dialect: Config.db.dialect,
     database: Config.db.name,
     username: Config.db.username,
     password: Config.db.password,
@@ -10,11 +10,23 @@ const sequelize = new Sequelize({
     host: Config.db.host,
 });
 
-const modelDefiners = [require('../model/User')];
+const modelDefiners = [
+    require('../models/user_supplier'),
+    require('../models/user_role'),
+    require('../models/user_recomendation_variable'),
+    require('../models/user'),
+    require('../models/survey_response'),
+    require('../models/supplier_location'),
+    require('../models/supplier'),
+    require('../models/recomendation_variable_catalog'),
+    require('../models/question'),
+    require('../models/product_category'),
+    require('../models/product'),
+];
 
 // We define all models according to their files.
 for (const modelDefiner of modelDefiners) {
-    modelDefiner(sequelize);
+    modelDefiner(sequelize, DataTypes);
 }
 
 module.exports = sequelize;
