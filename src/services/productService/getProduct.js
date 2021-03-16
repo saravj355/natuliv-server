c91524581f7100830a58ef1650cc45d7f65aef85
Sara Valle <svj971@gmail.com>
2021-03-16 14:20:04 -0500

    include related models on the product

--- src/services/productService/getProduct.js
+++ src/services/productService/getProduct.js
@@ -3,6 +3,16 @@ const ProductModel = models.product;
 
 async function getProduct(productId) {
     return ProductModel.findOne({
+        include: [
+            {
+                model: models.supplier,
+                as: 'supplier',
+            },
+            {
+                model: models.product_category,
+                as: 'productCategory',
+            },
+        ],
         where: {
             productId: productId,
         },
