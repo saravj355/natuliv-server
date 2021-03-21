4a2262a17bd2c1048c4e9898fb8ee467f7e9bf65
Sara Valle <svj971@gmail.com>
2021-03-21 12:06:55 -0500

    chore: remove unused service

--- src/services/supplierService/index.js
+++ src/services/supplierService/index.js
@@ -2,12 +2,10 @@ const getSupplier = require('./getSupplier');
 const getSuppliers = require('./getSuppliers');
 const createSupplier = require('./createSupplier');
 const updateSupplier = require('./updateSupplier');
-const updateSupplierStatus = require('./updateSupplierStatus');
 
 module.exports = {
     getSupplier,
     getSuppliers,
     createSupplier,
     updateSupplier,
-    updateSupplierStatus,
 };
