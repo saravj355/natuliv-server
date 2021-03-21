7a028a51e5314072aad88cb69986bf3e20fad3f2
Sara Valle <svj971@gmail.com>
2021-03-20 20:58:44 -0500

    remove unused service

--- src/services/userService/index.js
+++ src/services/userService/index.js
@@ -2,12 +2,10 @@ const createUser = require('./createUser');
 const getUser = require('./getUser');
 const getUsers = require('./getUsers');
 const updateUser = require('./updateUser');
-const updateUserStatus = require('./updateUserStatus');
 
 module.exports = {
     createUser,
     getUser,
     getUsers,
     updateUser,
-    updateUserStatus,
 };
