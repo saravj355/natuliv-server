d7f5aed161ce56d98d877ac8227775f60d6c162a
saravj355 <svj971@gmail.com>
2021-04-04 15:10:07 -0500

    Revert "chore: remove unused service"
    This reverts commit 4a2262a17bd2c1048c4e9898fb8ee467f7e9bf65.

--- src/services/productService/index.js
+++ src/services/productService/index.js
@@ -2,10 +2,12 @@ const getProducts = require('./getProducts');
 const getProduct = require('./getProduct');
 const createProduct = require('./createProduct');
 const updateProduct = require('./updateProduct');
+const updateProductStatus = require('./updateProductStatus');
 
 module.exports = {
     getProducts,
     createProduct,
     getProduct,
     updateProduct,
+    updateProductStatus,
 };
