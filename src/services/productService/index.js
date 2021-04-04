46e36a8045e9f91cf0ca2172191e8a3deceda7be
saravj355 <svj971@gmail.com>
2021-04-04 15:20:26 -0500

    remove unused service

--- src/services/productService/index.js
+++ src/services/productService/index.js
@@ -2,12 +2,10 @@ const getProducts = require('./getProducts');
 const getProduct = require('./getProduct');
 const createProduct = require('./createProduct');
 const updateProduct = require('./updateProduct');
-const updateProductStatus = require('./updateProductStatus');
 
 module.exports = {
     getProducts,
     createProduct,
     getProduct,
     updateProduct,
-    updateProductStatus,
 };
