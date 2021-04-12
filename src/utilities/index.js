97c52241c5e1684d40c381cfe7a2ebd5de0705ae
saravj355 <svj971@gmail.com>
2021-04-12 11:19:27 -0500

    add handle filters utility

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,5 +1,6 @@
 const UUID = require('./uuid');
 const Date = require('./date');
 const Hash = require('./hash');
+const handleFilters = require('./filters');
 
-module.exports = { UUID, Date, Hash };
+module.exports = { UUID, Date, Hash, handleFilters };
