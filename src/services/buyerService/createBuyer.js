69a5c6ab00df993287f26b487958a904972e82b0
saravj355 <svj971@gmail.com>
2021-04-19 12:32:46 -0500

    add create buyer

--- src/services/buyerService/createBuyer.js
+++ src/services/buyerService/createBuyer.js
@@ -2,12 +2,20 @@ const { models } = require('../../db');
 const BuyerUserModel = models.buyer_user;
 const identityUserService = require('../identityUserService');
 
-async function createBuyer({ buyerUser, identityUser }) {
-    const newIdentityUser = await identityUserService.createIdentityUser(
-        identityUser
-    );
-    buyerUser.identityUserId = newIdentityUser.id;
-    return BuyerUserModel.create(buyerUser);
+async function createBuyer(newBuyer) {
+    const newIdentityUser = {
+        email: newBuyer.email,
+        password: newBuyer.password,
+        identityUserRoleId: 2,
+    };
+
+    delete newBuyer.email;
+    delete newBuyer.password;
+
+    const identityUser = await identityUserService.create(newIdentityUser);
+
+    newBuyer.identityUserId = identityUser.id;
+    return BuyerUserModel.create(newBuyer);
 }
 
 module.exports = createBuyer;
