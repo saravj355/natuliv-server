88a23adcba030e93085b6be66bc7843d525002fb
saravj355 <svj971@gmail.com>
2021-04-21 15:52:20 -0500

    add random password

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -2,5 +2,6 @@ const UUID = require('./uuid');
 const Date = require('./date');
 const Hash = require('./hash');
 const Filters = require('./filters');
+const Random = require('./random');
 
-module.exports = { UUID, Date, Hash, Filters };
+module.exports = { UUID, Date, Hash, Filters, Random };
