39a62a5476e436c23f55b2fbb24c77dd7a0c5a36
saravj355 <svj971@gmail.com>
2021-04-22 11:38:14 -0500

    add create and get buyer

--- src/routes/api/buyers.js
+++ src/routes/api/buyers.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const buyerService = require('../../services/buyerService');
+const buyerService = require('../../services/buyer.service');
 
 /**
  * Get buyers
@@ -8,9 +8,43 @@ const buyerService = require('../../services/buyerService');
  */
 router.get('/', async (req, res) => {
     try {
-        const foundBuyers = await buyerService.findAll(req.query);
+        const foundBuyers = await buyerService.getBuyers(req.query);
 
         res.send(foundBuyers);
+    } catch (error) {
+        res.status(400).send(`An error ocurred: ${error}`);
+    }
+});
+
+/**
+ * Get buyer
+ * id: int
+ * @return Buyer || {}
+ */
+router.get('/:id', async (req, res) => {
+    try {
+        const foundBuyer = await buyerService.findBuyerById(req.params.id);
+
+        if (!foundBuyer) {
+            throw new Error('Buyer not found');
+        }
+
+        res.send(foundBuyer);
+    } catch (error) {
+        res.status(400).send(`An error ocurred: ${error}`);
+    }
+});
+
+/**
+ * Create buyer
+ * id: int
+ * @return Buyer
+ */
+router.post('/', async (req, res) => {
+    try {
+        const buyer = await buyerService.createBuyer(req.body);
+
+        res.send(buyer);
     } catch (error) {
         res.status(400).send(`An error ocurred: ${error}`);
     }
@@ -21,17 +55,20 @@ router.get('/', async (req, res) => {
  * id: int
  * @return Buyer || {}
  */
-router.put('/update/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
     try {
-        const foundBuyer = await buyerService.findOne(req.params.id);
+        const foundBuyer = await buyerService.findBuyerById(req.params.id);
 
         if (!foundBuyer) {
             throw new Error('Buyer not found');
         }
 
-        await buyerService.update(req.params.id, req.body);
+        const updatedBuyer = await buyerService.updateBuyer(
+            req.params.id,
+            req.body
+        );
 
-        res.send('Buyer has been updated');
+        res.send(updatedBuyer);
     } catch (error) {
         res.status(400).send(`An error ocurred: ${error}`);
     }
