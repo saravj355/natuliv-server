d1eecd306563fcae0bad48259cd68e100690bf6d
saravj355 <svj971@gmail.com>
2021-04-22 11:34:32 -0500

    fix identity service binding names

--- src/services/identityUserRole.service.js
+++ src/services/identityUserRole.service.js
@@ -1,12 +1,9 @@
 const { models } = require('../db');
 const IdentityUserRoleModel = models.identity_user_role;
-const { IdentityUserRoleEnum } = require('../enums');
 
 async function getRoleByKeyName(roleName) {
-    const keyName = IdentityUserRoleEnum[roleName];
-
     return IdentityUserRoleModel.findOne({
-        where: { keyName },
+        where: { keyName: roleName },
     });
 }
 
