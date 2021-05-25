e402cd1c7c575fa21c5007f76d2021084297187c
saravj355 <svj971@gmail.com>
2021-05-25 16:24:37 -0500

    remove unused route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
-const products = require('./products');
+
 const vendors = require('./vendors');
+const products = require('./products');
 const buyers = require('./buyers');
-const users = require('./users');
 
 //routes
-router.use('/products', products);
 router.use('/vendors', vendors);
+router.use('/products', products);
 router.use('/buyers', buyers);
-router.use('/users', users);
 
 module.exports = router;
