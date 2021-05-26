5c3c1e02d6b9acb6a8fc3f401de0d68ec21344b1
saravj355 <svj971@gmail.com>
2021-05-26 01:15:18 -0500

    remove random hash

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -2,6 +2,5 @@ const UUID = require('./uuid');
 const Date = require('./date');
 const Hash = require('./hash');
 const Filters = require('./filters');
-const Random = require('./random');
 
-module.exports = { UUID, Date, Hash, Filters, Random };
+module.exports = { UUID, Date, Hash, Filters };
--- src/utilities/random.js
+++
@@ -1,7 +0,0 @@
-const randomPassword = require('generate-password');
-
-function generate() {
-    return randomPassword.generate({ length: 10, numbers: true });
-}
-
-module.exports = { generate };
