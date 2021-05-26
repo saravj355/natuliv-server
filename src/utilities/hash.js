7c0fce0a982a97def39864cadca80aa2a2a4518e
saravj355 <svj971@gmail.com>
2021-05-26 01:15:37 -0500

    add random hash

--- src/utilities/hash.js
+++ src/utilities/hash.js
@@ -1,11 +1,34 @@
 const MD5 = require('crypto-js/md5');
+const randomPassword = require('generate-password');
+
+/**
+ * generate a password hash MD5 format
+ * @param {*} password: Required
+ * @returns password hash
+ */
 
 function generate(password) {
     return MD5(password).toString();
 }
 
+/**
+ * validate password hash equality
+ * @param {*} passwordHash: Required
+ * @param {*} password: Required
+ * @returns boolean
+ */
+
 function hashEqual(passwordHash, password) {
     return generate(password) === passwordHash;
 }
 
-module.exports = { generate, hashEqual };
+/**
+ * Get a random password hash
+ * @returns password
+ */
+
+function random() {
+    return generate(randomPassword.generate({ length: 10, numbers: true }));
+}
+
+module.exports = { generate, hashEqual, random };
